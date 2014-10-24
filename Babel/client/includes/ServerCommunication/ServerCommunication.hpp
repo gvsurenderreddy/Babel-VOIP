0378ff8a0255749b8f8c6729b84c8739d1436490
Simon Ninon <simon.ninon@gmail.com>
2014-10-25 00:20:40 +0200

    prepare the handling of the received commands in the ServerCommunication class. (empty) Functions have been declared

--- Babel/client/includes/ServerCommunication/ServerCommunication.hpp
+++ Babel/client/includes/ServerCommunication/ServerCommunication.hpp
@@ -8,6 +8,30 @@ class ServerCommunication : public QObject {
 
 	Q_OBJECT
 
+	// handle commands
+	private:
+		struct HandleCommand {
+			ICommand::Instruction	instruction;
+			void					(ServerCommunication::*handler)(const ICommand *command);
+		};
+
+		static const ServerCommunication::HandleCommand	handleCommandsTab[];
+
+		void	handleRegCommand(const ICommand *command);
+		void	handleLogCommand(const ICommand *command);
+		void	handleListCommand(const ICommand *command);
+		void	handleShowCommand(const ICommand *command);
+		void	handleAddCommand(const ICommand *command);
+		void	handleAcceptAddCommand(const ICommand *command);
+		void	handleDelCommand(const ICommand *command);
+		void	handleExitCommand(const ICommand *command);
+		void	handeUpdateCommand(const ICommand *command);
+		void	handleSendCommand(const ICommand *command);
+		void	handleCallCommand(const ICommand *command);
+		void	handleAcceptCallCommand(const ICommand *command);
+		void	handleCloseCallCommand(const ICommand *command);
+		void	handleErrCommand(const ICommand *command);
+
 	// ctor - dtor
 	public:
 		ServerCommunication(void);
--- Babel/client/sources/ServerCommunication/ServerCommunication.cpp
+++ Babel/client/sources/ServerCommunication/ServerCommunication.cpp
@@ -1,5 +1,22 @@
 #include "ServerCommunication.hpp"
 
+const ServerCommunication::HandleCommand	ServerCommunication::handleCommandsTab[] = {
+	{ ICommand::REG, &ServerCommunication::handleRegCommand },
+	{ ICommand::LOG, &ServerCommunication::handleLogCommand },
+	{ ICommand::LIST, &ServerCommunication::handleListCommand },
+	{ ICommand::SHOW, &ServerCommunication::handleShowCommand },
+	{ ICommand::ADD, &ServerCommunication::handleAddCommand },
+	{ ICommand::ACCEPT_ADD, &ServerCommunication::handleAcceptAddCommand },
+	{ ICommand::DEL, &ServerCommunication::handleDelCommand },
+	{ ICommand::EXIT, &ServerCommunication::handleExitCommand },
+	{ ICommand::UPDATE, &ServerCommunication::handeUpdateCommand },
+	{ ICommand::SEND, &ServerCommunication::handleSendCommand },
+	{ ICommand::CALL, &ServerCommunication::handleCallCommand },
+	{ ICommand::ACCEPT_CALL, &ServerCommunication::handleAcceptCallCommand },
+	{ ICommand::CLOSE_CALL, &ServerCommunication::handleCloseCallCommand },
+	{ ICommand::ERR, &ServerCommunication::handleErrCommand }
+};
+
 ServerCommunication::ServerCommunication(void) {
 	connect(&mCommandPacketBuilder, SIGNAL(receiveCommand(const ICommand *)), this, SLOT(treatCommand(const ICommand *)));
 }
@@ -12,4 +29,46 @@ void	ServerCommunication::treatCommand(const ICommand *) {
 
 void	ServerCommunication::connectToServer(const QString &addr, int port) {
 	mCommandPacketBuilder.connectToServer(addr, port);
+}
+
+void	ServerCommunication::handleRegCommand(const ICommand *command) {
+}
+
+void	ServerCommunication::handleLogCommand(const ICommand *command) {
+}
+
+void	ServerCommunication::handleListCommand(const ICommand *command) {
+}
+
+void	ServerCommunication::handleShowCommand(const ICommand *command) {
+}
+
+void	ServerCommunication::handleAddCommand(const ICommand *command) {
+}
+
+void	ServerCommunication::handleAcceptAddCommand(const ICommand *command) {
+}
+
+void	ServerCommunication::handleDelCommand(const ICommand *command) {
+}
+
+void	ServerCommunication::handleExitCommand(const ICommand *command) {
+}
+
+void	ServerCommunication::handeUpdateCommand(const ICommand *command) {
+}
+
+void	ServerCommunication::handleSendCommand(const ICommand *command) {
+}
+
+void	ServerCommunication::handleCallCommand(const ICommand *command) {
+}
+
+void	ServerCommunication::handleAcceptCallCommand(const ICommand *command) {
+}
+
+void	ServerCommunication::handleCloseCallCommand(const ICommand *command) {
+}
+
+void	ServerCommunication::handleErrCommand(const ICommand *command) {
 }
