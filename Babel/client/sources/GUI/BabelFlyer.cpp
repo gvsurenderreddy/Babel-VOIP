fa6087ec69c599a2d75faf940d1606b33c4397fa
Davy TRAN <davy.tran@epitech.eu>
2014-10-25 01:23:03 +0200

    update BabelFlyer(ui|cpp)

--- Babel/client/sources/GUI/BabelFlyer.cpp
+++ Babel/client/sources/GUI/BabelFlyer.cpp
@@ -3,6 +3,12 @@
 BabelFlyer::BabelFlyer(void)
 {
 	mUi.setupUi(this);
+
+	// Remove window borders
+	setWindowFlags(Qt::Window | Qt::CustomizeWindowHint | Qt::FramelessWindowHint);
+
+	// Desable resize window
+	setWindowFlags(windowFlags() | Qt::MSWindowsFixedSizeDialogHint);
 }
 
 BabelFlyer::~BabelFlyer(void)
