d8373d32a6b4aeb5a300d1eb520e164e0ab4068f
Simon Ninon <simon.ninon@gmail.com>
2014-10-26 16:03:18 +0100

    patch value of the instructions codes

--- Babel/client/includes/Commands/ICommand.hpp
+++ Babel/client/includes/Commands/ICommand.hpp
@@ -9,20 +9,20 @@ class ICommand {
 	// Instructions
 	public:
 		enum Instruction {
-			REG,
-			LOG,
-			LIST,
-			SHOW,
-			ADD,
-			ACCEPT_ADD,
-			DEL,
-			EXIT,
-			UPDATE,
-			SEND,
-			CALL,
-			ACCEPT_CALL,
-			CLOSE_CALL,
-			ERR,
+			REG		= 0x01,
+			LOG		= 0x02,
+			LIST		= 0x03,
+			SHOW		= 0x04,
+			ADD		= 0x05,
+			ACCEPT_ADD	= 0x06,
+			DEL		= 0x07,
+			EXIT		= 0x08,
+			UPDATE		= 0x09,
+			SEND		= 0x0A,
+			CALL		= 0x0B,
+			ACCEPT_CALL	= 0x0C,
+			CLOSE_CALL	= 0x0D,
+			ERR		= 0x0E,
 			UNKNOWN_INSTRUCTION
 		};
 
