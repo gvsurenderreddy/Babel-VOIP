26082c234f2adda0d8f09430eb04d38e1df6032c
Rainiugnas <pguego@gmail.fr>
2014-10-29 12:03:37 +0100

    change function name GetContact => LIST + improve Update

--- Babel/server/includes/Client.hpp
+++ Babel/server/includes/Client.hpp
@@ -23,7 +23,7 @@ public:
         virtual bool onSubscribe(const std::string &acount, const std::string& password) = 0;
 		virtual bool onConnect(const std::string &account, const std::string &password) = 0;
 		virtual void onDisconnect(const std::string &account, const std::string &pseudo, char status,const std::list<std::string> &contact) = 0;
-		virtual const std::string &onGetContact(const std::list<std::string> &contacts) = 0;
+		virtual void onList(const std::list<std::string> &contacts) = 0;
 		virtual bool onUpdate(const std::string &account, const std::string &password, const std::string &currentAccount) = 0;
 		virtual bool onAddContact(const std::string &account) = 0;
 		virtual bool onDelContact(const std::string &account) = 0;
