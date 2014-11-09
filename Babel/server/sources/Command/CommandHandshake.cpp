5d0ae930b497176030b5d6e7a7d99bcea220d1e4
Simon Ninon <simon.ninon@gmail.com>
2014-11-09 19:34:04 +0100

    patch compilation warning under linux

--- Babel/server/sources/Command/CommandHandshake.cpp
+++ Babel/server/sources/Command/CommandHandshake.cpp
@@ -1,13 +1,12 @@
 #include "Command/CommandHandshake.hpp"
 
-std::vector<std::string>		*CommandHandshake::getParam(IClientSocket *){
+std::vector<std::string>		*CommandHandshake::getParam(IClientSocket *) {
     return new std::vector<std::string>;
 }
 
-IClientSocket::Message			*CommandHandshake::setParam(const std::vector<std::string> &param){
-
-    CommandHandshake::BodySend		*bodySend = new CommandHandshake::BodySend;
-    IClientSocket::Message		*msg = new IClientSocket::Message;
+IClientSocket::Message		*CommandHandshake::setParam(const std::vector<std::string> &) {
+    CommandHandshake::BodySend	*bodySend = new CommandHandshake::BodySend;
+    IClientSocket::Message	*msg = new IClientSocket::Message;
 
     std::memset(bodySend, 0, sizeof(CommandHandshake::BodySend));
     bodySend->header.instructionCode = ICommand::HANDSHAKE;
@@ -16,9 +15,9 @@ IClientSocket::Message			*CommandHandshake::setParam(const std::vector<std::stri
     msg->msgSize = sizeof(CommandHandshake::BodySend);
     msg->msg = reinterpret_cast<char *>(bodySend);
 
-    return (msg);
+    return msg;
 }
 
-unsigned int					CommandHandshake::getSizeBody(void){
+unsigned int	CommandHandshake::getSizeBody(void) {
 	return 0;
 }
